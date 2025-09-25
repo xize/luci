9a57f740792f26e11d3aeca1f13ab96a160e6399
xize <glucassen@live.nl>
2025-09-25 15:35:02 +0200

    [luci]: update updata-amnezia.sh

--- protocols/update-amnezia.sh
+++ protocols/update-amnezia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git remote add amnezia https://github.com/amnezia-vpn/amnezia-openwrt-luci
+git remote add amnezia https://github.com/amnezia-vpn/amneziawg-openwrt
 git fetch amnezia
-git checkout -f amnezia/luci-proto-amneziawg luci-proto-amneziawg
+git checkout -f amnezia luci-proto-amneziawg
 exit 0
